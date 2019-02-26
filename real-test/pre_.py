eea29c71f85ddea40d47638befc0728b8bf1cf25
Herixth <herixth@gmail.com>
2019-02-26 15:47:09 +0800

    update 提升切片性能

--- real-test/pre_.py
+++ real-test/pre_.py
@@ -5,15 +5,16 @@ from PIL import Image
 import PIL.ImageOps
 import sys
 import os
+import math
 
 class Image_helper:
     __path = ''
     __tmp  = '/src.tmp'
     __height = 0
     __width = 0
-    __threthod = 165
-    __gard = 80
-    __pad = 5
+    __threthod = 125
+    __gard = 111
+    __pad = 6
     __smpic = []
     
     def __init__(self, *args, **kwargs):
@@ -26,65 +27,61 @@ class Image_helper:
     def binary(self):
         table = [int(elem >= self.__threthod) for elem in range(256)]
         self.__pic.point(table, '1')
-
-    def cut(self, height, width):
-        self.__height, self.__width = height, width
         w, h = self.__pic.size
-        flg = True
-        while flg:
-            flg = False
-            cluster_y_x = []
-            FX, FY, SX, SY = 0, 0, 0, 0
-            for x in range(w):
-                for y in range(h):
-                    if self.__pic.getpixel((x, y)) > self.__gard:
-                        flg = True
-                        FX = x
-                        cluster_y_x.append(y)
-                        break
-                if flg:
-                    break
-            if flg == False:
-                break
+        for x in range(w):
             for y in range(h):
-                for x in range(w):
-                    if self.__pic.getpixel((x, y)) > self.__gard:
-                        FY = y
-                        flg = False
-                        cluster_y_x.append(x)
-                        break
-                if flg == False:
+                pix = self.__pic.getpixel((x, y))
+                if pix < self.__gard:
+                    self.__pic.putpixel((x, y), 0)
+                elif pix < 200:
+                    self.__pic.putpixel((x, y), pix + 50)
+        self.__pic.save(self.__path.replace('png', 'jpg'))
+
+    def __col_cut(self):
+        w, h = self.__pic.size
+        front, rec = 0, False
+        for col in range(w):
+            __is_edge = False
+            for row in range(h):
+                if self.__pic.getpixel((col, row)) > self.__gard:
+                    __is_edge = True
                     break
-            # for SX SY
-            flg = True
-            for x in range(cluster_y_x[1], w):
-                for y in range(cluster_y_x[0] + 1, h):
-                    flg = True
-                    # current (x, y)
-                    for dx in range(FX, x + 1):
-                        if self.__pic.getpixel((dx, y)) > self.__gard:
-                            flg = False
-                            break
-                    if flg == False:
-                        continue
-                    for dy in range(FY, y + 1):
-                        if self.__pic.getpixel((x, dy)) > self.__gard:
-                            flg = False
-                            break
-                    if flg == False:
-                        break
-                    SX, SY = x, y
+            if __is_edge == False:
+                if rec == True:
+                    self.__smpic.append(self.__pic.crop((front, 0, col, h)))
+                    rec = False
+            else:                
+                if rec == False:
+                    front = col
+                    rec = True
+                elif rec == True and col == w - 1:
+                    self.__smpic.append(self.__pic.crop((front, 0, col, h)))
+
+    def __mid_func(self, pic):
+        w, h = pic.size
+        top, rec = 0, False
+        for y in range(h):
+            __is_edge = False
+            for x in range(w):
+                if pic.getpixel((x, y)) > self.__gard:
+                    __is_edge = True
                     break
-                if flg:
-                    break
-            flg = True
-            print FX, FY, SX, SY
-            sm = self.__pic.crop((FX, FY, SX, SY))
-            sm = sm.resize((height - 2 * self.__pad, width - 2 * self.__pad), Image.ANTIALIAS)
-            newImg = Image.new('RGB', (SX - FX, SY - FY), 'black')
-            self.__pic.paste(newImg, (FX, FY, SX, SY))
-            # [TODO] digit '1' crop unsuit
-            self.__smpic.append(sm)
+            if __is_edge == True and rec == False:
+                top = y
+                rec = True
+            elif __is_edge == False and rec == True:
+                return pic.crop((0, top, w, y))
+                # return pic.resize((self.__height - 2 * self.__pad, self.__width - 2 * self.__pad), Image.ANTIALIAS)
+            elif rec == True and __is_edge == True and y == h - 1:
+                return pic.crop((0, top, w, y))
+   
+    def __row_cut(self):
+        self.__smpic = list(map(self.__mid_func, self.__smpic))
+    
+    def cuts(self, height, width):
+        self.__height, self.__width = height, width
+        self.__col_cut()
+        self.__row_cut()
 
     def save(self):
         # save pic
@@ -92,9 +89,17 @@ class Image_helper:
         cnt = 0
         for pic in self.__smpic:
             cnt += 1
+            print 'Image No.', cnt
+            sw, sh = pic.size
+            rate = 1.0 * (self.__height - 2 * self.__pad) / max(sw, sh)
+            pic = pic.resize((int(math.ceil(sw * rate)), int(math.ceil(sh * rate))))
+            
+            sw, sh = pic.size
             self.__base = Image.new('RGB', (self.__height, self.__width)).convert('L')
-            self.__base.paste(pic, (self.__pad, self.__pad, self.__width - self.__pad, self.__height - self.__pad))
-            self.__base.save(self.__path.replace('canvas.png', str(cnt) + '.jpg'))
+            FX, FY = self.__width / 2 - sw / 2, self.__height / 2 - sh / 2
+            self.__base.paste(pic, (FX, FY, FX + sw, FY + sh))
+            # self.__base.paste(pic, (self.__pad, self.__pad, self.__width - self.__pad, self.__height - self.__pad))
+            self.__base.save(self.__path.replace('canvas.png', 'split/' + str(cnt) + '.jpg'))
             # save numbers
             with open(sys.path[0] + '\\' + self.__tmp, "w") as outFile:
                 for h in range(0, self.__height):
@@ -102,12 +107,13 @@ class Image_helper:
                         pixel = self.__base.getpixel((w, h))
                         outFile.write(str(pixel) + ' ')
             os.system('.\Rec_digit.exe src.tmp res.txt')
+            os.system('del src.tmp')
     
 def main():
     image = Image_helper()
     image.open_pic()
     image.binary()
-    image.cut(28, 28)
+    image.cuts(28, 28)
     image.save()
 
 if __name__ == '__main__':
